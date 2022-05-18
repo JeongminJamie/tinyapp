0770570f7a9b25b03e59e98ea5b46cb965a1ce40
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-18 19:11:36 -0400

    login route

--- express_server.js
+++ express_server.js
@@ -4,16 +4,6 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const alphanumeric = '123456ABCDEFGabcdefg';
-function generateRandomString(times) {
-  let result = '';
-  const alphanumericLength = alphanumeric.length;
-  for (let i = 0; i < times; i++) {
-    result += alphanumeric.charAt(Math.floor(Math.random() * alphanumericLength));
-  }
-  return result;
-};
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set("view engine", "ejs");
@@ -36,13 +26,36 @@ const users = {
   }
 }
 
+const alphanumeric = '123456ABCDEFGabcdefg';
+function generateRandomString(times) {
+  let result = '';
+  const alphanumericLength = alphanumeric.length;
+  for (let i = 0; i < times; i++) {
+    result += alphanumeric.charAt(Math.floor(Math.random() * alphanumericLength));
+  }
+  return result;
+};
+
+const emailLookup = function (email) {
+  for (const key in users) {
+    if (users[key].email === email) {
+      return users[key];
+    }
+  }
+  return false;
+};
+
 app.get("/urls/new", (req, res) => {
-  const loginId = { user: users[req.cookies["user_id"]] };
+  const user_id = req.cookies["user_id"];
+  const user = users[user_id] || {};
+  const loginId = { user: user };
   res.render("urls_new", loginId);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies["user_id"]] };
+  const user_id = req.cookies["user_id"];
+  const user = users[user_id] || {};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: user };
   res.render("urls_show", templateVars);
 });
 
@@ -69,12 +82,18 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
+  const user_id = req.cookies["user_id"];
+  const user = users[user_id] || {};
+  const templateVars = { urls: urlDatabase, user: user };
   res.render("urls_index", templateVars);
 });
 
 app.get("/register", (req, res) => {
   res.render("urls_register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("urls_login")
 });
 
 app.post("/urls", (req, res) => {
@@ -97,24 +116,15 @@ app.post("/urls/:id", (req, res) => {
   res.redirect("/urls");
 });
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect("/urls");
-});
+// app.post("/login", (req, res) => {
+//   res.cookie('username', req.body.username);
+//   res.redirect("/urls");
+// });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   res.redirect("/urls");
 });
-
-const emailLookup = function (email) {
-  for (const key in users) {
-    if (users[key].email === email) {
-      return true;
-    }
-  }
-  return false;
-};
 
 app.post("/register", (req, res) => {
   const randomId = generateRandomString(6);
@@ -122,13 +132,30 @@ app.post("/register", (req, res) => {
   if (email === "" || password === "") {
     return res.status(400).send({ message: "email or password is invalid" });
   };
-  let lookupResult = emailLookup(email);
-  if (lookupResult) {
-    return res.status(400).send({ message: "email in use" });
+  let emailLookupResult = emailLookup(email);
+  if (!emailLookupResult) {
+    users[randomId] = { id: randomId, email: email, password: password };
+    res.cookie("user_id", randomId);
+    res.redirect("/urls");
+    return;
   }
-  users[randomId] = { id: randomId, email: email, password: password };
-  res.cookie("user_id", randomId);
-  res.redirect("/urls");
+  res.status(400).send({ message: "email in use" });
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  let currentUser = emailLookup(email);
+  if (!currentUser) {
+    return res.status(403).send("e-mail cannot be found");
+  }
+  if (currentUser.password !== password) {
+    return res.status(403).send("Your password is wrong:(")
+  }
+  if (currentUser.password === password) {
+    res.cookie("user_id", currentUser.id)
+    return res.redirect("/urls");
+  }
+  return res.send("your email or password is wrong!")
 });
 
 app.listen(PORT, () => {
